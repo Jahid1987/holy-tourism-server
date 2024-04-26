a455dc69f158cb5e314b55b9da46f58797ae3b86
Jahid Anwer <jahid.englaa@gmail.com>
2024-04-26 16:14:32 +0600

    exress, mongodb, cors added and database connected

---
+++ index.js
@@ -0,0 +1,47 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6iad9fh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // Get the database and collection on which to run the operation
+    const database = client.db("tourismDB");
+    const usersCollection = database.collection("users");
+
+    // reading users
+    app.get("/users", async (req, res) => {
+      const result = usersCollection.find();
+      const users = await result.toArray();
+      res.send(users);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
